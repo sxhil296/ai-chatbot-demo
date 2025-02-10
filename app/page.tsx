568488a5307321e6ea6aaea127b82596e1f3d401
sahil <leviack629@gmail.com>
2025-02-10 22:52:59 +0530

    route and initial msg

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,42 @@
 "use client";
-
+import { useChat } from "@ai-sdk/react";
 import { AnimatePresence, motion } from "framer-motion";
 import LandingSections from "@/components/LandingSections";
 import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   ArrowBigDown,
+  Loader2,
   MessageCircle,
   MessageSquareCodeIcon,
+  Send,
   X,
 } from "lucide-react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Input } from "@/components/ui/input";
 
 export default function Chat() {
   const [showChatIcon, setShowChatIcon] = useState<boolean>(false);
   const [isChatOpen, setIsChatOpen] = useState<boolean>(false);
   const chatIconRef = useRef<HTMLButtonElement>(null);
+
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    isLoading,
+    stop,
+    reload,
+    error,
+  } = useChat({ api: "/api/gemini" });
 
   useEffect(() => {
     const handleScroll = () => {
@@ -76,16 +97,83 @@ export default function Chat() {
           >
             <Card className="border-2">
               <CardHeader className="flex justify-between items-center w-full flex-row space-y-0 pb-3">
-                <CardTitle className="text-lg font-medium">Chat with AI</CardTitle>
+                <CardTitle className="text-lg font-medium">
+                  Chat with AI
+                </CardTitle>
 
-                <Button onClick={toggleChat} variant={"ghost"} size={"sm"} className="py-0 px-2">
+                <Button
+                  onClick={toggleChat}
+                  variant={"ghost"}
+                  size={"sm"}
+                  className="py-0 px-2"
+                >
                   <X className="size-4" />
                   <span className="sr-only">Close Chat</span>
                 </Button>
               </CardHeader>
               <CardContent className="p-0">
-
+                <ScrollArea className="h-[300px] pr-4">
+                  {messages?.length === 0 && (
+                    <div className="w-full mt-32 text-gray-500 items-center justify-center flex gap-3">
+                      No messages yet
+                    </div>
+                  )}
+                  {messages?.map((message, index) => (
+                    <div
+                      key={index}
+                      className="flex flex-col items-start gap-2 px-4 py-3 text-sm"
+                    >
+                      skdjvh
+                    </div>
+                  ))}
+                  {isLoading && (
+                    <div className="w-full items-center flex justify-center gap-3">
+                      <Loader2 className="animate-spin h-5 w-5 text-primary" />
+                      <button
+                        className="underline"
+                        type="button"
+                        onClick={() => stop()}
+                      >
+                        Abort
+                      </button>
+                    </div>
+                  )}
+                  {error && (
+                    <div className="w-full items-center flex justify-center gap-3">
+                   <p className="text-xs text-red-500">An error occured!</p>
+                    <button
+                      className="underline"
+                      type="button"
+                      onClick={() => reload()}
+                    >
+                      Retry
+                    </button>
+                  </div>
+                  )}
+                </ScrollArea>
               </CardContent>
+              <CardFooter>
+                <form
+                  onSubmit={handleSubmit}
+                  className="flex w-full items-center space-x-2"
+                >
+                  <Input
+                    value={input}
+                    onChange={handleInputChange}
+                    className="flex-1"
+                    required
+                    placeholder="Type your message here..."
+                  />
+                  <Button
+                    type="submit"
+                    className="size-9"
+                    disabled={isLoading}
+                    size={"icon"}
+                  >
+                    <Send className="size-4" />
+                  </Button>
+                </form>
+              </CardFooter>
             </Card>
           </motion.div>
         )}
---
+++ lib/data.ts
@@ -0,0 +1,20 @@
+export const initialMessage = {
+  role: "system",
+  content: `
+        You are Fydbox AI, the intelligent assistant for Fydbox – an anonymous feedback platform that makes collecting and managing feedback seamless and efficient. Your role is to assist users with everything related to Fydbox, ensuring they have a smooth and intuitive experience. 
+
+ Core Features of Fydbox:
+        - Generate personalized feedback links & QR codes  
+        - Control access: Set feedback as public or private  
+        - Enable/disable feedback links anytime  
+        - Delete feedback whenever needed  
+        - Create unlimited feedback forms  
+        - Receive anonymous messages and manage feedback effortlessly  
+
+
+      Stay concise, helpful, and user-friendly in responses, ensuring users can efficiently manage their feedback experience on Fydbox. Do not answer the questions that are not related to FydBox.
+      
+
+      Please format your responses using markdown.
+      `,
+};
